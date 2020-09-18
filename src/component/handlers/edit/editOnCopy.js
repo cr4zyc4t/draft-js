84f3e0c01fe3cac5d4e978eb4126640149b733b1
ngocnhu123 <nhuwhn96@gmail.com>
2020-09-18 16:45:02 +0700

    update to version 1.11.8

--- src/component/handlers/edit/editOnCopy.js
+++ src/component/handlers/edit/editOnCopy.js
@@ -1,18 +1,21 @@
 /**
- * Copyright (c) Facebook, Inc. and its affiliates.
+ * Copyright (c) 2013-present, Facebook, Inc.
+ * All rights reserved.
  *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
  *
  * @format
  * @flow strict-local
- * @emails oncall+draft_js
  */
 
 'use strict';
 
 import type DraftEditor from 'DraftEditor.react';
 
+const ContentState = require('ContentState');
+const convertFromDraftStateToRaw = require('convertFromDraftStateToRaw');
 const getFragmentFromSelection = require('getFragmentFromSelection');
 
 /**
@@ -30,7 +33,31 @@ function editOnCopy(editor: DraftEditor, e: SyntheticClipboardEvent<>): void {
     return;
   }
 
-  editor.setClipboard(getFragmentFromSelection(editor._latestEditorState));
+  const fragment = getFragmentFromSelection(editor._latestEditorState);
+
+  editor.setClipboard(fragment);
+
+  // IE11 does not support ClipboardEvent.clipboardData.
+  if (e.clipboardData && fragment) {
+    const content = ContentState.createFromBlockArray(fragment.toArray());
+    const serialisedContent = JSON.stringify(
+      convertFromDraftStateToRaw(content),
+    );
+
+    const fragmentElt = document.createElement('div');
+    const domSelection = window.getSelection();
+    fragmentElt.appendChild(domSelection.getRangeAt(0).cloneContents());
+    fragmentElt.setAttribute('data-editor-content', serialisedContent);
+    // We set the style property to replicate the browser's behavior of inline
+    // styles in rich text copy-paste. This is important for line breaks to be
+    // interpreted correctly when pasted into another word processor.
+    fragmentElt.setAttribute('style', 'white-space: pre-wrap;');
+
+    e.clipboardData.setData('text/plain', domSelection.toString());
+    e.clipboardData.setData('text/html', fragmentElt.outerHTML);
+
+    e.preventDefault();
+  }
 }
 
 module.exports = editOnCopy;
--- src/model/paste/DraftPasteProcessor.js
+++ src/model/paste/DraftPasteProcessor.js
@@ -1,30 +1,33 @@
 /**
- * Copyright (c) Facebook, Inc. and its affiliates.
+ * Copyright (c) 2013-present, Facebook, Inc.
+ * All rights reserved.
  *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
  *
  * @format
  * @flow
- * @emails oncall+draft_js
  */
 
 'use strict';
 
 import type {BlockNodeRecord} from 'BlockNodeRecord';
-import type CharacterMetadata from 'CharacterMetadata';
 import type {DraftBlockRenderMap} from 'DraftBlockRenderMap';
 import type {DraftBlockType} from 'DraftBlockType';
 import type {EntityMap} from 'EntityMap';
 
+const CharacterMetadata = require('CharacterMetadata');
 const ContentBlock = require('ContentBlock');
 const ContentBlockNode = require('ContentBlockNode');
+const Immutable = require('immutable');
 
-const convertFromHTMLToContentBlocks = require('convertFromHTMLToContentBlocks');
+const convertFromHTMLtoContentBlocksClassic = require('convertFromHTMLToContentBlocks');
+const convertFromHTMLtoContentBlocksNew = require('convertFromHTMLToContentBlocks2');
+const convertFromRawToDraftState = require('convertFromRawToDraftState');
 const generateRandomKey = require('generateRandomKey');
 const getSafeBodyFromHTML = require('getSafeBodyFromHTML');
 const gkx = require('gkx');
-const Immutable = require('immutable');
 const sanitizeDraftText = require('sanitizeDraftText');
 
 const {List, Repeat} = Immutable;
@@ -34,16 +37,40 @@ const ContentBlockRecord = experimentalTreeDataSupport
   ? ContentBlockNode
   : ContentBlock;
 
+const refactoredHTMLImporter = gkx('draft_refactored_html_importer');
+const convertFromHTMLtoContentBlocks = refactoredHTMLImporter
+  ? convertFromHTMLtoContentBlocksNew
+  : convertFromHTMLtoContentBlocksClassic;
+
 const DraftPasteProcessor = {
   processHTML(
     html: string,
     blockRenderMap?: DraftBlockRenderMap,
-  ): ?{
-    contentBlocks: ?Array<BlockNodeRecord>,
-    entityMap: EntityMap,
-    ...
-  } {
-    return convertFromHTMLToContentBlocks(
+  ): ?{contentBlocks: ?Array<BlockNodeRecord>, entityMap: EntityMap} {
+    const body = getSafeBodyFromHTML(html);
+    const fragmentElt = body && body.querySelector('[data-editor-content]');
+    const fragmentAttr =
+      (fragmentElt && fragmentElt.getAttribute('data-editor-content')) || null;
+
+    // Handle the paste without converting the HTML if it comes from another Draft.js editor.
+    if (fragmentAttr) {
+      let rawContent;
+
+      try {
+        // If JSON parsing fails, handle the paste as normal HTML.
+        rawContent = JSON.parse(fragmentAttr);
+      } catch (error) {}
+
+      if (rawContent) {
+        const content = convertFromRawToDraftState(rawContent);
+        return {
+          contentBlocks: content.getBlocksAsArray(),
+          entityMap: content.getEntityMap(),
+        };
+      }
+    }
+
+    return convertFromHTMLtoContentBlocks(
       html,
       getSafeBodyFromHTML,
       blockRenderMap,
